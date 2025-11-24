439b26396faf0fc1f26437785c9d5af9844f87c8
HADI <152994917+Spicyicee@users.noreply.github.com>
2025-11-24 15:52:12 +0400

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,18 +10,15 @@ var cacheFiles = [
     'images/sandjpeg.jpeg',
     'images/icon-store-512.png',
 ];
-
-self.addEventListener('install', (e) =>{
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) =>{
-            console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFiles);
-        })
-    );
-
-});
-
+// self.addEventListener('install', (e) =>{
+//     console.log('[ServiceWorker] Install');
+//     e.waitUntil(
+//         caches.open(cacheName).then((cache) =>{
+//             console.log('[Service Worker] Caching all the files');
+//             return cache.addAll(cacheFiles);
+//         })
+//     );
+// });
 self.addEventListener('fetch', function (e){
     e.respondWith(
         caches.match(e.request).then(function (r){
@@ -31,10 +28,6 @@ self.addEventListener('fetch', function (e){
                     return response;
                 });
             });
-
         })
-
     );
-    
-
 });
