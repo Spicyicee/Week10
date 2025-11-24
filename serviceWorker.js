b530d91eddad72f0dea632f691cf12105762722d
HADI <152994917+Spicyicee@users.noreply.github.com>
2025-11-24 15:43:55 +0400

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -25,7 +25,7 @@ self.addEventListener('install', (e) =>{
 self.addEventListener('fetch', function (e){
     e.respondWith(
         caches.match(e.request).then(function (r){
-            return r || fetch(e.request).then(function(repsponse){
+            return r || fetch(e.request).then(function(response){
                 return caches.open(cacheName).then(function (cache){
                     cache.put(e.request, response.clone());
                     return response;
